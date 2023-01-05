abc2bf018715be3bf07443ecfe67508e1a9dfb20
mutai-gilbert <mutaigilbert85@gmail.com>
2023-01-05 12:25:59 +0300

    Made changes to the api.js file

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import { getAPI, setAPI } from './modules/api.js';
 import display from './modules/display.js';
 
 const refresh = document.querySelector('.refresh-button');
-const submitButton = document.querySelector('.submit-button');
+const form = document.querySelector('form');
 
 const getScore = async () => {
   const scores = await getAPI();
   display(scores);
 };
-submitButton.addEventListener('submit', async (event) => {
+form.addEventListener('submit', async (event) => {
   event.preventDefault();
   const userName = document.querySelector('#name');
   const userScore = document.querySelector('#score');
--- src/modules/api.js
+++ src/modules/api.js
@@ -1,18 +1,16 @@
-const gameId = 'Zl4d7IVkemOTTVg2fUdz';
-
 // Add score
 
 const getAPI = async () => {
-  const response = await fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameId}/scores/`);
+  const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/Rz215bgYBb0lSnJ7gUWT/scores/');
   const result = await response.json();
   return result;
 };
 
-const setAPI = async (score) => {
-  const response = await fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameId}/scores/`, {
+const setAPI = async (scores) => {
+  const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/Rz215bgYBb0lSnJ7gUWT/scores/', {
     method: 'POST',
-    body: JSON.stringify({ score }),
     headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(scores),
   });
   const result = await response.json();
   return result;
